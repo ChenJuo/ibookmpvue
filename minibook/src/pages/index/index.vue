7d6370402b68c359f412f2a43383a7b6384ca9e1
chenzhuo <jingxiaofeng1023@gmail.com>
2020-05-19 16:36:56 +0800

    banner组件开发

--- minibook/src/pages/index/index.vue
+++ minibook/src/pages/index/index.vue
@@ -4,10 +4,13 @@
       disable
       @onClick="onSearchBarkClick"
     />
-  <!--  <ImageView
-      src="https://www.youbaobao.xyz/mpvue-res/big.jpg"
-    />-->
     <HomeCard/>
+    <HomeBanner
+      img="http://www.youbaobao.xyz/book/res/bg.jpg"
+      title="mpvue2.0实战多端小程序上线啦"
+      subTitle="立即体验"
+      @onClick="onBannerClick"
+    />
   </div>
 </template>
 
@@ -15,8 +18,10 @@
   import SearchBar from '../../components/home/SearchBar'
   import ImageView from "../../components/base/imageView";
   import HomeCard from "../../components/home/HomeCard";
+  import HomeBanner from "../../components/home/HomeBanner";
   export default {
     components:{
+      HomeBanner,
       HomeCard,
       ImageView,
       SearchBar
@@ -24,6 +29,9 @@
     methods: {
       onSearchBarkClick(){
         //跳转到搜索页面
+      },
+      onBannerClick(){
+        console.log('banner click……')
       }
     }
   };
