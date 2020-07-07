779aa5adbcd89f18ff4323c4d2dba3b7f937eafb
chenzhuo <jingxiaofeng1023@gmail.com>
2020-07-07 14:42:49 +0800

    书架图书图书评分功能开发

--- minibook/src/api/index.js
+++ minibook/src/api/index.js
@@ -46,3 +46,8 @@ export function hotSearch() {
 export function bookDetail(params) {
   return get(`${API_URL}/book/detail`,params)
 }
+
+export function bookRankSave(params) {
+  return get(`${API_URL}/book/rank/save`,params)
+}
+
--- minibook/src/pages/detail/detail.vue
+++ minibook/src/pages/detail/detail.vue
@@ -1,16 +1,30 @@
 <template>
   <div>
     <DetailBook :book="book"/>
+    <DetailStat
+      :readers="book.readers"
+      :readerNum="book.readerNum"
+      :rankNum="book.rankNum"
+      :rankAvg="book.rankAvg"
+    />
+    <DetailRate
+      :rate-value="book.rateValue"
+      @onRateChange="onRateChange"
+    />
   </div>
 </template>
 
 <script>
   import DetailBook from '../../components/detail/DetailBook'
-  import {bookDetail} from "../../api";
+  import DetailStat from '../../components/detail/DetailStat'
+  import DetailRate from "../../components/detail/DetailRate";
+  import {bookDetail,bookRankSave} from "../../api";
   import {getStorageSync} from "../../api/wechat";
 
   export default {
     components:{
+      DetailRate,
+      DetailStat,
       DetailBook
     },
     data(){
@@ -18,16 +32,30 @@
         book:{}
       }
     },
-    mounted(){
-      const openId = getStorageSync('openId')
-      const { fileName } = this.$route.query
-      console.log('detail',openId,fileName)
-      if(openId && fileName){
+    methods:{
+      onRateChange(value){
+        const openId = getStorageSync('openId')
+        const {fileName} = this.$route.query
+        bookRankSave({
+          openId,fileName,rank:value
+        }).then(res =>{
+          this.getBookDetail()
+        })
+      },
+      getBookDetail(){
+        const openId = getStorageSync('openId')
+        const { fileName } = this.$route.query
+        console.log('detail',openId,fileName)
+        if(openId && fileName){
           bookDetail({openId,fileName}).then(res =>{
             this.book = res.data.data
             console.log(res)
           })
+        }
       }
+    },
+    mounted(){
+      this.getBookDetail()
     }
   }
 </script>
